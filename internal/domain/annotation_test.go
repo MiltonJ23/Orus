5ba3f88832cdf784d5efb18291d6563bebcb6534
MiltonJ23 <jonhmilton1991@gmail.com>
2026-02-16 06:55:01 +0100

    Created and tested the method to create a new bookmark, tested the errors to be returned and finally the type of bookmark that should be obtained. Got more than 90 % of coverage

---
+++ internal/domain/annotation_test.go
@@ -0,0 +1,50 @@
+package domain_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/MiltonJ23/Orus/internal/domain"
+)
+
+func TestNewBookMark(t *testing.T) {
+	// i guess i have to create a new bookmark first , let's go
+	_, bookmarkCreationError := domain.NewBookMark("3ubiosdfo24", domain.AnnotationBookmark, 30)
+	if bookmarkCreationError != nil {
+		t.Fatalf("expected no errors, got %v", bookmarkCreationError)
+	}
+
+	// now let's test the case of improper bookID
+	_, bookmarkCreationWrongBookId := domain.NewBookMark("", domain.AnnotationBookmark, 30)
+	if !errors.Is(bookmarkCreationWrongBookId, domain.ErrInvalidBookId) {
+		t.Errorf("expected error to be ErrInvalidBookId, instead got %v", bookmarkCreationWrongBookId)
+	}
+
+	// now let's test the case of the improper page Number
+	_, pageNumberErr := domain.NewBookMark("3ubiosdfo24", domain.AnnotationBookmark, -2)
+	if !errors.Is(pageNumberErr, domain.ErrInvalidPageNumber) {
+		t.Errorf("expected error to be ErrInvalidPageNumber, instead got %v", pageNumberErr)
+	}
+
+}
+
+func TestBookMarkType(t *testing.T) {
+	// we are checking if the book got the correct value for the bookmark type
+	bookmark, bookmarkCreationError := domain.NewBookMark("kln934nalj4fs", domain.AnnotationBookmark, 30)
+	if bookmarkCreationError != nil {
+		t.Fatalf("expected no errors, got %v", bookmarkCreationError)
+	}
+
+	if bookmark.Type != "bookmark" {
+		t.Fatalf("bookmark.Type should  be 'bookmark', instead got %v", bookmark.Type)
+	}
+	// now let's cover for the other type of bookmark
+	bookmark2, bookmarkCreationError2 := domain.NewBookMark("kln934nalj4fs", domain.AnnotationHighlight, 30)
+	if bookmarkCreationError2 != nil {
+		t.Fatalf("expected no errors, got %v", bookmarkCreationError2)
+	}
+
+	if bookmark2.Type != "highlight" {
+		t.Fatalf("bookmark.Type should be 'hightlight', instead got %v", bookmark2.Type)
+	}
+}
