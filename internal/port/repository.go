cf048371d7ea044881cb5a3dc4563ccdebc2fbf9
MiltonJ23 <jonhmilton1991@gmail.com>
2026-02-17 02:52:52 +0100

    removing the pointers  declaration for context argument in all the methods of the contract

--- internal/port/repository.go
+++ internal/port/repository.go
@@ -8,14 +8,14 @@ import (
 
 // BookRepository defines the contract to implement for persistence operations related to Books.
 type BookRepository interface {
-	Save(ctx *context.Context, book *domain.Book) error
-	GetByID(ctx *context.Context, id int) (*domain.Book, error)
-	ListAll(ctx *context.Context) ([]*domain.Book, error)
-	Delete(ctx *context.Context, id string) error
+	Save(ctx context.Context, book *domain.Book) error
+	GetByID(ctx context.Context, id int) (*domain.Book, error)
+	ListAll(ctx context.Context) ([]*domain.Book, error)
+	Delete(ctx context.Context, id string) error
 }
 
 // SessionRepository defines the contract to implement for persistence operations related to Reading Sessions.
 type SessionRepository interface {
-	Save(ctx *context.Context, session *domain.ReadingSession) error
-	GetByID(ctx *context.Context, bookID string) (*domain.ReadingSession, error)
+	Save(ctx context.Context, session *domain.ReadingSession) error
+	GetByID(ctx context.Context, bookID string) (*domain.ReadingSession, error)
 }
