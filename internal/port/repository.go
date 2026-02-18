2be8014a63802c4b20bbc52d264bf7b2d4b60634
MiltonJ23 <jonhmilton1991@gmail.com>
2026-02-18 07:44:04 +0100

    Renaming the others contract method to avoid confusion. Modified the contract of the SessionRepository. Added the contract for the AnnotationRepository

--- internal/port/repository.go
+++ internal/port/repository.go
@@ -9,13 +9,21 @@ import (
 // BookRepository defines the contract to implement for persistence operations related to Books.
 type BookRepository interface {
 	Save(ctx context.Context, book *domain.Book) error
-	GetByID(ctx context.Context, id int) (*domain.Book, error)
+	GetByID(ctx context.Context, id string) (*domain.Book, error)
 	ListAll(ctx context.Context) ([]*domain.Book, error)
-	Delete(ctx context.Context, id string) error
+	Delete(ctx context.Context, bookId string) error
 }
 
 // SessionRepository defines the contract to implement for persistence operations related to Reading Sessions.
 type SessionRepository interface {
-	Save(ctx context.Context, session *domain.ReadingSession) error
-	GetByID(ctx context.Context, bookID string) (*domain.ReadingSession, error)
+	SaveSession(ctx context.Context, session *domain.ReadingSession) error
+	GetSessionByID(ctx context.Context, bookID string) (*domain.ReadingSession, error)
+}
+
+type AnnotationRepository interface {
+	SaveAnnotation(ctx context.Context, annotation *domain.Annotation) error
+	GetAnnotationByPage(ctx context.Context, pageNo int) ([]*domain.Annotation, error)
+	GetAnnotationByType(ctx context.Context, annotationType string) ([]*domain.Annotation, error)
+	DeleteAnnotation(ctx context.Context, id string) error
+	ListAllAnnotation(ctx context.Context) ([]*domain.Annotation, error)
 }
